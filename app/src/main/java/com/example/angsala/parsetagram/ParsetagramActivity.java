2d2dcad41fdb3b4623cd88fb8bfbae3eb5359824
Angela Salacata <angsala@fb.com>
2018-07-10 08:53:01 -0700

    Added activity to create new account

--- app/src/main/java/com/example/angsala/parsetagram/ParsetagramActivity.java
+++ app/src/main/java/com/example/angsala/parsetagram/ParsetagramActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.parse.LogInCallback;
 import com.parse.ParseException;
@@ -14,49 +15,65 @@ import com.parse.ParseUser;
 
 public class ParsetagramActivity extends AppCompatActivity {
 
-   final String TAG = "ParsetagramActivity";
-    EditText loginUsername;
-    EditText loginPassword;
-    Button loginButton;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_parsetagram);
+  final String TAG = "ParsetagramActivity";
+  EditText loginUsername;
+  EditText loginPassword;
+  TextView createAccount;
+  Button loginButton;
 
-        //inflate
-        loginUsername = (EditText) findViewById(R.id.loginUsername);
-        loginPassword = (EditText) findViewById(R.id.loginPassword);
-        loginButton = (Button) findViewById(R.id.loginButton);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_parsetagram);
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                final String passedusername = loginUsername.getText().toString();
-                final String passedpassword = loginPassword.getText().toString();
-                login_helper(passedusername,passedpassword);
-            }
-        });
-    }
+    // inflate
+    loginUsername = (EditText) findViewById(R.id.loginUsername);
+    loginPassword = (EditText) findViewById(R.id.loginPassword);
+    loginButton = (Button) findViewById(R.id.loginButton);
+    createAccount = (TextView) findViewById(R.id.txtvCreateAccount);
 
-    //helper function to login in from click listener
-    private void login_helper(String username, String password){
-        //call the log in methods and pass in username and password. LogIn call back will let us know when network request comes back!
-        ParseUser.logInInBackground(username, password, new LogInCallback() {
-            @Override
-            public void done(ParseUser user, ParseException e) {
-                if (user != null){
-                    Log.d(TAG, "Login successful");
-                     Intent intent = new Intent(ParsetagramActivity.this, HomeActivity.class);
-                     startActivity(intent);
-                     finish();
-
-                } else{
-                    Log.e(TAG, "Failed to login");
-                    e.printStackTrace();
-                }
-            }
+    loginButton.setOnClickListener(
+        new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+            final String passedusername = loginUsername.getText().toString();
+            final String passedpassword = loginPassword.getText().toString();
+            login_helper(passedusername, passedpassword);
+          }
         });
 
-    }
+    createAccount.setOnClickListener(
+        new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+            Intent intent = new Intent(ParsetagramActivity.this, CreateAccountActivity.class);
+            startActivity(intent);
+            finish();
+          }
+        });
+  }
 
+  // helper function to login in from click listener
+  private void login_helper(String username, String password) {
+    // call the log in methods and pass in username and password. LogIn call back will let us know
+    // when network request comes back!
+    ParseUser.logInInBackground(
+        username,
+        password,
+        new LogInCallback() {
+          @Override
+          public void done(ParseUser user, ParseException e) {
+            if (user != null) {
+              Log.d(TAG, "Login successful");
+              Intent intent = new Intent(ParsetagramActivity.this, HomeActivity.class);
+              startActivity(intent);
+              finish();
+
+            } else {
+              Log.e(TAG, "Failed to login");
+              e.printStackTrace();
+            }
+          }
+        });
+  }
 }

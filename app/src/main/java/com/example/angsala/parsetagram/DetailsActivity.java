b7e53900714f942a3c552df30bc0cb782a25ceed
Angela Salacata <angsala@fb.com>
2018-07-11 16:48:28 -0700

    Details page has description

--- app/src/main/java/com/example/angsala/parsetagram/DetailsActivity.java
+++ app/src/main/java/com/example/angsala/parsetagram/DetailsActivity.java
@@ -1,25 +1,19 @@
 package com.example.angsala.parsetagram;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.angsala.parsetagram.models.Post;
-import com.parse.GetCallback;
-import com.parse.ParseException;
-import com.parse.ParseQuery;
 
 import org.parceler.Parcels;
 
 public class DetailsActivity extends AppCompatActivity {
     ImageView detailsImage;
     TextView detailsDescription;
-    String objectId;
     String textDescription;
-
+    String textUsername;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,25 +24,27 @@ public class DetailsActivity extends AppCompatActivity {
 
     final Post receivedPost = (Post) Parcels.unwrap(getIntent().getParcelableExtra(Post.class.getSimpleName()));
 
-        Intent intent = getIntent();
-        objectId = intent.getStringExtra(PostAdapter.POST_ID);
+       // Intent intent = getIntent();
+       // objectId = intent.getStringExtra(PostAdapter.POST_ID);
 
 
-        ParseQuery<Post> postsQuery = ParseQuery.getQuery(Post.class);
-
-        postsQuery.getInBackground(
-        objectId,
-        new GetCallback<Post>() {
-          @Override
-          public void done(Post post, ParseException e) {
-            if (e == null) {
-              textDescription = post.getDescription();
-            } else {
-              Toast.makeText(DetailsActivity.this, "Failed to extract description", Toast.LENGTH_SHORT).show();
-            }
-          }
-        });
-
+//        ParseQuery<Post> postsQuery = ParseQuery.getQuery(Post.class);
+//
+//        postsQuery.getInBackground(
+//        objectId,
+//        new GetCallback<Post>() {
+//          @Override
+//          public void done(Post post, ParseException e) {
+//            if (e == null) {
+//              textDescription = post.getDescription();
+//            } else {
+//              Toast.makeText(DetailsActivity.this, "Failed to extract description", Toast.LENGTH_SHORT).show();
+//            }
+//          }
+//        });
+        textDescription = receivedPost.getDescription();
         detailsDescription.setText(textDescription);
+
+
     }
 }
